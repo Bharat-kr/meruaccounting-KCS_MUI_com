ee953ca27e0f7e6a989d56ba46ebc45a773448a0
ayushsnofi <cosmosofficial53@gmail.com>
2021-12-07 23:18:45 +0530

    LoginAuth Added

--- front-end/src/components/privatroutes.js
+++ front-end/src/components/privatroutes.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, Navigate } from 'react-router-dom';
 
-import { authenticationService } from '@/_services';
+// import { authenticationService } from '../_services/authetication.service';
+import { LoginContext } from '../contexts/LoginContext';
 
-export const PrivateRoute = ({ component: Component, roles, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) => {
-      const currentUser = authenticationService.currentUserValue;
-      if (!currentUser) {
-        // not logged in so redirect to login page with the return url
-        return <Redirect to={{ pathname: '/login', state: { from: props.location } }} />;
-      }
-
-      // check if route is restricted by role
-      if (roles && roles.indexOf(currentUser.role) === -1) {
-        // role not authorised so redirect to home page
-        return <Redirect to={{ pathname: '/' }} />;
-      }
-
-      // authorised so return component
-      return <Component {...props} />;
-    }}
-  />
-);
+export const PrivateRoute = ({ component: Component, roles, ...rest }) => {
+  const LoginC = LoginContext();
+  const ud = JSON.parse(localStorage.getItem('ud'));
+  const islogin = { ...LoginC };
+  console.log(ud);
+  return islogin ? ud.role === roles && <Component /> : <Navigate to="/" />;
+};
