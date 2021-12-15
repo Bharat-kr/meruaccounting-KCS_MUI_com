7683afa551e1d3d1e89b219102edc134fc7fe558
Kamal <kamal021099@gmail.com>
2021-12-15 20:05:27 +0530

    added jsx a11y as plugin

--- front-end/src/_helpers/role.js
+++ front-end/src/_helpers/role.js
@@ -1,8 +1,1 @@
-// export const Role = {
-//   SuperAdmin: 'superadmin',
-//   Admin: 'admin',
-//   Manage: 'manager',
-//   Teamleader: 'teamleader',
-//   Employee: 'employee'
-// };
 export const Role = ['superadmin', 'admin', 'manager', 'teamleader', 'employee'];
