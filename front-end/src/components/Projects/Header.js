51a3256c1314f9c3001f6123f3af238a49f58c2f
ayushsnofi <cosmosofficial53@gmail.com>
2022-01-03 19:18:17 +0530

    Project page improvements

--- front-end/src/components/Projects/Header.js
+++ front-end/src/components/Projects/Header.js
@@ -6,9 +6,12 @@ import {
   Autocomplete,
   TextField,
   Link,
+  Button,
+  Fab,
 } from "@mui/material";
+
+import FloatingForm from "../_dashboard/muicomponents/FloatingForm";
 import SearchBar from "../SearchBar";
-import CollapsibleTable from "../Projects/ProjectMemers";
 import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 import { TreeItem, TreeView } from "@mui/lab";
 import Treeview from "../Treeview";
@@ -21,7 +24,9 @@ import { ClientsContext } from "../../contexts/ClientsContext";
 import { getClientProjects } from "../../api/clients api/clients";
 import AddIcon from "@mui/icons-material/Add";
 import { display } from "@mui/material/node_modules/@mui/system";
+import EnhancedTable from "../Projects/ProjectMemers";
 //---------------------------------------------------------------
+
 const useStyles = makeStyles((theme) => ({
   input: {
     color: "#000",
@@ -38,6 +43,41 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 export default function Header() {
+  const columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "firstName", headerName: "First name", width: 130 },
+    { field: "lastName", headerName: "Last name", width: 130 },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      width: 90,
+    },
+    {
+      field: "fullName",
+      headerName: "Full name",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      valueGetter: (params) =>
+        `${params.getValue(params.id, "firstName") || ""} ${
+          params.getValue(params.id, "lastName") || ""
+        }`,
+    },
+  ];
+
+  const rows = [
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  ];
+
   const classes = useStyles();
   // to focus edit name of client
   const inputRef = useRef();
@@ -169,6 +209,7 @@ export default function Header() {
               </Paper>
 
               <SearchBar
+                label="Assign Project Leader"
                 handleSearch={handleSearch}
                 id="combo-box-demo"
                 options={["Ayush", "Shrey", "Kamal", "Bharat"]}
@@ -190,10 +231,72 @@ export default function Header() {
 
             <Paper elevation={2} sx={{ pt: 1 }}>
               <hr />
-              <Typography variant="h5" sx={{ pt: 1 }}>
-                Project Members
-              </Typography>
-              <CollapsibleTable />
+              <Box
+                sx={{
+                  display: "flex ",
+                  justifyContent: "space-between",
+                  pt: 2,
+                  pb: 2,
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex ",
+                    alignItems: "center",
+                  }}
+                >
+                  <Typography variant="h5" sx={{ pt: 1 }}>
+                    Project Members
+                  </Typography>
+                  <FloatingForm
+                    toolTip="Add Member"
+                    color="primary"
+                    icon={<AddIcon />}
+                  >
+                    <form
+                      // onSubmit={handleSubmit}
+                      noValidate
+                      autoComplete="off"
+                      style={{ padding: "10px" }}
+                    >
+                      <TextField
+                        // onChange={(e) => setNewTeam(e.target.value)}
+                        required
+                        fullWidth
+                        label="Add new Member"
+                        // error={newClientError}
+                        sx={{}}
+                      />
+                      <Button
+                        fullWidth
+                        type="submit"
+                        variant="contained"
+                        sx={{ mt: 1 }}
+                      >
+                        Submit
+                      </Button>
+                    </form>
+                  </FloatingForm>
+                </div>
+                <div
+                  style={{
+                    display: "inherit",
+                    width: "300px",
+                    alignItems: "center",
+                  }}
+                >
+                  <SearchBar
+                    label="Search Member"
+                    handleSearch={handleSearch}
+                    id="combo-box-demo"
+                    options={["Ayush", "Shrey", "Kamal", "Bharat"]}
+                    renderInput={(params) => (
+                      <TextField {...params} label="Select New Team Leader" />
+                    )}
+                  />
+                </div>
+              </Box>
+              <EnhancedTable />
             </Paper>
           </Box>
         </Paper>
