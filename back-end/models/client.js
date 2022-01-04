d404968ce52acc285e352c4ee0b1d935bfb19338
Ayush Garg <ayush181000@gmail.com>
2022-01-04 16:35:28 +0530

    DB edits

--- back-end/models/client.js
+++ back-end/models/client.js
@@ -1,9 +1,13 @@
 import mongoose from 'mongoose';
 
-const clientSchema = new mongoose.Schema({
-  name: { type: String },
-  projects: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Project' }],
-});
+const clientSchema = new mongoose.Schema(
+  {
+    name: { type: String },
+    projects: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Project' }],
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  },
+  { timestamps: true }
+);
 
 const Client = mongoose.model('Client', clientSchema);
 
--- back-end/models/project.js
+++ back-end/models/project.js
@@ -1,13 +1,17 @@
 import mongoose from 'mongoose';
 
-const projectSchema = new mongoose.Schema({
-  name: { type: String },
-  budgetedHours: { type: Number },
-  projectLeader: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  employees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  client: { type: mongoose.Schema.Types.ObjectId, ref: 'Client' },
-  // team: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Team' }],
-});
+const projectSchema = new mongoose.Schema(
+  {
+    name: { type: String },
+    budgetedHours: { type: Number },
+    projectLeader: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    employees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    client: { type: mongoose.Schema.Types.ObjectId, ref: 'Client' },
+    // team: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Team' }],
+  },
+  { timestamps: true }
+);
 
 const Project = mongoose.model('Project', projectSchema);
 
