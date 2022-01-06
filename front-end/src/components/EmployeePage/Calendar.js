758f3bd6baa9f2935408c811063b826a2675adaa
Bharat Kumar <kumarbharat661@gmail.com>
2022-01-06 18:37:51 +0530

    employee page corrected and link styling made to default

--- front-end/src/components/EmployeePage/Calendar.js
+++ front-end/src/components/EmployeePage/Calendar.js
@@ -1,22 +1,328 @@
-import * as React from 'react';
-import TextField from '@mui/material/TextField';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import DatePicker from '@mui/lab/DatePicker';
+import React from "react";
+import moment from "moment";
+import Box from "@mui/material/Box";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
+import "./calendar.css";
+export default class Calendar extends React.Component {
+  weekdayshort = moment.weekdaysShort();
 
-export default function BasicDatePicker() {
-  const [value, setValue] = React.useState(null);
+  state = {
+    showYearTable: false,
+    showMonthTable: false,
+    showDateTable: true,
+    dateObject: moment(),
+    allmonths: moment.months(),
+    selectedDay: null,
+  };
+  daysInMonth = () => {
+    return this.state.dateObject.daysInMonth();
+  };
+  year = () => {
+    return this.state.dateObject.format("Y");
+  };
+  currentDay = () => {
+    return this.state.dateObject.format("D");
+  };
+  firstDayOfMonth = () => {
+    let dateObject = this.state.dateObject;
+    let firstDay = moment(dateObject).startOf("month").format("d"); // Day of week 0...1..5...6
+    return firstDay;
+  };
+  month = () => {
+    return this.state.dateObject.format("MMMM");
+  };
+  showMonth = (e, month) => {
+    this.setState({
+      showMonthTable: !this.state.showMonthTable,
+      showDateTable: !this.state.showDateTable,
+    });
+  };
+  setMonth = (month) => {
+    let monthNo = this.state.allmonths.indexOf(month);
+    let dateObject = Object.assign({}, this.state.dateObject);
+    dateObject = moment(dateObject).set("month", monthNo);
+    this.setState({
+      dateObject: dateObject,
+      showMonthTable: !this.state.showMonthTable,
+      showDateTable: !this.state.showDateTable,
+    });
+  };
+  MonthList = (props) => {
+    let months = [];
+    props.data.map((data) => {
+      months.push(
+        <td
+          key={data}
+          className="calendar-month"
+          onClick={(e) => {
+            this.setMonth(data);
+          }}
+        >
+          <span>{data}</span>
+        </td>
+      );
+    });
+    let rows = [];
+    let cells = [];
 
-  return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <DatePicker
-        label="Basic example"
-        value={value}
-        onChange={(newValue) => {
-          setValue(newValue);
-        }}
-        renderInput={(params) => <TextField {...params} />}
-      />
-    </LocalizationProvider>
-  );
+    months.forEach((row, i) => {
+      cells.push(row);
+    });
+    rows.push(cells);
+    let monthlist = rows.map((d, i) => {
+      return <tr key={i}>{d}</tr>;
+    });
+
+    return (
+      <table className="calendar-month">
+        <thead>
+          <tr>
+            <th colSpan="100%">Select a Month</th>
+          </tr>
+        </thead>
+        <tbody>{monthlist}</tbody>
+      </table>
+    );
+  };
+  showYearTable = (e) => {
+    this.setState({
+      showYearTable: !this.state.showYearTable,
+      showDateTable: !this.state.showDateTable,
+    });
+  };
+
+  onPrev = () => {
+    let curr = "";
+    if (this.state.showYearTable === true) {
+      curr = "year";
+    } else {
+      curr = "month";
+    }
+    this.setState({
+      dateObject: this.state.dateObject.subtract(1, curr),
+    });
+  };
+  onNext = () => {
+    let curr = "";
+    if (this.state.showYearTable === true) {
+      curr = "year";
+    } else {
+      curr = "month";
+    }
+    this.setState({
+      dateObject: this.state.dateObject.add(1, curr),
+    });
+  };
+  setYear = (year) => {
+    // alert(year)
+    let dateObject = Object.assign({}, this.state.dateObject);
+    dateObject = moment(dateObject).set("year", year);
+    this.setState({
+      dateObject: dateObject,
+      showMonthTable: !this.state.showMonthTable,
+      showYearTable: !this.state.showYearTable,
+    });
+  };
+  onYearChange = (e) => {
+    this.setYear(e.target.value);
+  };
+  getDates(startDate, stopDate) {
+    var dateArray = [];
+    var currentDate = moment(startDate);
+    var stopDate = moment(stopDate);
+    while (currentDate <= stopDate) {
+      dateArray.push(moment(currentDate).format("YYYY"));
+      currentDate = moment(currentDate).add(1, "year");
+    }
+    return dateArray;
+  }
+  YearTable = (props) => {
+    let months = [];
+    let nextten = moment().set("year", props).add("year", 12).format("Y");
+
+    let tenyear = this.getDates(props, nextten);
+
+    tenyear.map((data) => {
+      months.push(
+        <td
+          key={data}
+          className="calendar-month"
+          onClick={(e) => {
+            this.setYear(data);
+          }}
+        >
+          <span>{data}</span>
+        </td>
+      );
+    });
+    let rows = [];
+    let cells = [];
+
+    months.forEach((row, i) => {
+      cells.push(row);
+    });
+    rows.push(cells);
+    let yearlist = rows.map((d, i) => {
+      return <tr key={i}>{d}</tr>;
+    });
+
+    return (
+      <table className="calendar-month">
+        <thead>
+          <tr>
+            <th colSpan="100%">Select a Year</th>
+          </tr>
+        </thead>
+        <tbody>{yearlist}</tbody>
+      </table>
+    );
+  };
+  onDayClick = (e, d) => {
+    this.setState(
+      {
+        selectedDay: d,
+      },
+      () => {
+        console.log("SELECTED DAY: ", this.state.selectedDay);
+      }
+    );
+  };
+  render() {
+    let dayofDate = [];
+    for (let d = 1; d <= this.daysInMonth(); d++) {
+      let currentDay = d == this.currentDay() ? "today" : "";
+      dayofDate.push(
+        <th key={d} className={`calendar-day ${currentDay}`}>
+          {moment(`${this.year()}-${this.month()}-${d}`, "YYYY-MMMM-DD")
+            .format("dddd")
+            .slice(0, 3)}
+        </th>
+      );
+    }
+    let daysInMonth = [];
+    let trackingData = [];
+    for (let d = 1; d <= this.daysInMonth(); d++) {
+      let currentDay = d == this.currentDay() ? "today" : "";
+      daysInMonth.push(
+        <td key={d} className={`calendar-day ${currentDay}`}>
+          <span
+            onClick={(e) => {
+              this.onDayClick(e, d);
+            }}
+          >
+            {d}
+          </span>
+          {/* <div style={{height:"15px", width: "10%", backgroundColor: "#007B55" }}></div> */}
+        </td>
+      );
+      trackingData.push(
+        <td
+          className="hoursCells"
+          key={d}
+          style={{
+            borderWidth: "0 1px 0 1px",
+            borderStyle: "solid",
+            borderColor:"#C4CDD5",
+            height: "15px",
+            pointerEvents:"none"
+          }}
+        >
+          <div style={{height:"100%", width: `${d*3}%`, backgroundColor: "#007B55" }}></div>
+        </td>
+      );
+    }
+    let dayOfDateRow = [];
+    let dayOfDateCells = [];
+    var totalSlots = [...daysInMonth];
+    let rows = [];
+    dayofDate.forEach((row, i) => {
+      dayOfDateCells.push(row);
+      if (i === totalSlots.length - 1) {
+        dayOfDateRow.push(dayOfDateCells);
+      }
+    });
+    let weekdayshortname = dayOfDateRow.map((d, i) => {
+      return <tr key={i}>{d}</tr>;
+    });
+    let cells = [];
+
+    totalSlots.forEach((row, i) => {
+      cells.push(row);
+      if (i === totalSlots.length - 1) {
+        rows.push(cells);
+      }
+    });
+
+    let daysinmonth = rows.map((d, i) => {
+      return <tr key={i}>{d}</tr>;
+    });
+
+    return (
+      <div className="tail-datetime-calendar">
+        <Box
+          className="calendar-navi"
+          sx={{
+            backgroundColor: "primary.main",
+            "@media (max-width: 800px)": {
+              width: "100%",
+            },
+          }}
+        >
+          <span
+            onClick={(e) => {
+              this.onPrev();
+            }}
+            className="calendar-button button-prev"
+          >
+            <ArrowLeftIcon />
+          </span>
+          {!this.state.showMonthTable && (
+            <span
+              onClick={(e) => {
+                this.showMonth();
+              }}
+              className="calendar-label"
+            >
+              {this.month()}
+            </span>
+          )}
+          <span
+            className="calendar-label"
+            onClick={(e) => this.showYearTable()}
+          >
+            {this.year()}
+          </span>
+          <span
+            onClick={(e) => {
+              this.onNext();
+            }}
+            className="calendar-button button-next"
+          >
+            <ArrowRightIcon />
+          </span>
+        </Box>
+        <div className="calendar-date">
+          {this.state.showYearTable && <this.YearTable props={this.year()} />}
+        </div>
+        <div className="calendar-date">
+          {this.state.showMonthTable && (
+            <this.MonthList data={moment.months()} />
+          )}
+        </div>
+
+        {this.state.showDateTable && (
+          <div className="calendar-date">
+            <table className="calendar-day">
+              <thead>{weekdayshortname}</thead>
+              <tbody>
+                {daysinmonth}
+                <tr>{trackingData.map((e) => e)}</tr>
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
--- front-end/src/components/EmployeePage/Overview.js
+++ front-end/src/components/EmployeePage/Overview.js
@@ -1,75 +1,131 @@
 import {
-  Container,
-  Box,
-  Paper,
   Typography,
   CardContent,
   Card,
-  CardActions,
-  Button,
-  Grid,
+  Box,
+  Divider
 } from "@mui/material";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
 import React from "react";
+import moment from "moment";
 import {
   Monthlyhours,
   Weeklyhours,
   AppItemOrders,
   AppBugReports,
 } from "../_dashboard/app";
-import Calendar from "./Calendar";
 
 export default function Overview(props) {
   // console.log(props.employee);
+  const [value, setValue] = React.useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <Box
-      component="div"
+    component="div"
+    sx={{
+      m: 2,
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      "@media (max-width: 780px)": {
+        flexDirection: "column",
+      },
+    }}
+  >
+    <Box
       sx={{
-        m: 2,
+        width: "40%",
+        "@media (max-width: 780px)": {
+          width: "100%",
+          marginBottom: "10px",
+        },
         display: "flex",
         flexDirection: "row",
-        justifyContent: "center",
+        flexWrap: "wrap",
+        justifyContent: "space-evenly",
       }}
     >
-      {/* <Calendar /> */}
-      <Container maxWidth="lg">
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6} md={3}>
-            <Monthlyhours />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <Weeklyhours />
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <AppItemOrders />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <AppBugReports />
-          </Grid>
-        </Grid>
-      </Container>
-      {/* <Card elevation={3} sx={{ minWidth: 275 }}>
-        <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Friday, December 3
-          </Typography>
-          <Typography variant="h4" component="div">
-            1h 32m
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            Current Project
-          </Typography>
-          <Typography variant="body2">
-            Week 9h 48m
-            <br />
-            Month 4h 19m
-          </Typography>
-        </CardContent>
-      </Card> */}
-
-      {/* <Card elevation={3} sx={{ width: '80%' }}>
-        HEllo
-      </Card> */}
+      <AppItemOrders />
+      <Weeklyhours />
+      <Monthlyhours />
+      <AppBugReports />
     </Box>
+    <Box
+      sx={{
+        width: "60%",
+        "@media (max-width: 780px)": {
+          width: "100%",
+        },
+      }}
+    >
+      <Card
+        elevation={3}
+        sx={{
+          minWidth: 275,
+          height: "100%",
+          backgroundColor: "info.lighter",
+        }}
+      >
+        <CardContent>
+          <TabContext value={value}>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.primary"
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                {moment().format("MMMM Do YYYY, h:mm:ss a")}
+                <TabList
+                  onChange={handleChange}
+                  aria-label="lab API tabs example"
+                >
+                  <Tab label="Tasks" value="1" />
+                  <Tab label="Apps & URL's" value="2" />
+                </TabList>
+              </Typography>
+            </Box>
+            <TabPanel value="1">
+              <Box overflow={"auto"}>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Typography
+                    sx={{ mb: 1.5 }}
+                    variant="h5"
+                    color="text.primary"
+                  >
+                    Current Project
+                    <br />
+                    <Typography color="text.primary">
+                      Tasks getting Performed
+                    </Typography>
+                  </Typography>
+                  <Typography variant="h4" component="div">
+                    1h 32m
+                  </Typography>
+                </Box>
+                <Divider sx={{ backgroundColor: "primary.dark" }} />
+              </Box>
+            </TabPanel>
+            <TabPanel value="2">...</TabPanel>
+          </TabContext>
+        </CardContent>
+      </Card>
+    </Box>
+  </Box>
   );
 }
