68f153fa07c2b849a423d2f141b149e7476473c1
Prakhar <srivastava11735@gmail.com>
2022-01-10 18:03:51 +0530

    Manager Middle Ware

--- back-end/routes/employee.js
+++ back-end/routes/employee.js
@@ -1,5 +1,6 @@
-import express from 'express';
-import { authPass } from '../middleware/authMiddleware.js';
+import express from "express";
+import { authPass } from "../middleware/authMiddleware.js";
+import { managerPass } from "../middleware/roleMiddleware.js";
 import {
   addEmployee,
   getEmployeeById,
@@ -7,17 +8,17 @@ import {
   deleteEmployee,
   editEmployee,
   getEmployeeList,
-} from '../controllers/employee.js';
+} from "../controllers/employee.js";
 
 const router = express.Router();
 
-import moment from 'moment';
+import moment from "moment";
 moment().format();
 
-router.route('/add/:id').patch(addEmployee);
-router.route('/:id').get(getEmployeeById).delete(deleteEmployee);
-router.route('/').post(createEmployee);
-router.route('/edit/:id').patch(authPass, editEmployee);
-router.route('/employeeList').get(authPass, getEmployeeList);
+router.route("/add/:id").patch(addEmployee);
+router.route("/:id").get(getEmployeeById).delete(managerPass, deleteEmployee);
+router.route("/").post(createEmployee);
+router.route("/edit/:id").patch(authPass, editEmployee);
+router.route("/employeeList").get(authPass, getEmployeeList);
 
 export default router;
