880518912930eb38f52a6c9baa12ab4c72c7f25a
Ayush Garg <ayush181000@gmail.com>
2022-01-13 13:51:50 +0530

    custom cursor

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,17 @@
 // routes
-import Router from "./routes";
+import Router from './routes';
 // theme
-import ThemeConfig from "./theme";
-import GlobalStyles from "./theme/globalStyles";
+import ThemeConfig from './theme';
+import GlobalStyles from './theme/globalStyles';
+import AnimatedCursor from 'react-animated-cursor';
 // components
-import ScrollToTop from "./components/ScrollToTop";
-import { BaseOptionChartStyle } from "./components/charts/BaseOptionChart";
-import { LoginProvider } from "./contexts/LoginContext";
-import { TeamsProvider } from "./contexts/TeamsContext";
-import { ClientsContextProvider } from "./contexts/ClientsContext";
-import { EmployeeProvider } from "./contexts/EmployeeContext";
-import { ProjectsContextProvider } from "./contexts/ProjectsContext";
+import ScrollToTop from './components/ScrollToTop';
+import { BaseOptionChartStyle } from './components/charts/BaseOptionChart';
+import { LoginProvider } from './contexts/LoginContext';
+import { TeamsProvider } from './contexts/TeamsContext';
+import { ClientsContextProvider } from './contexts/ClientsContext';
+import { EmployeeProvider } from './contexts/EmployeeContext';
+import { ProjectsContextProvider } from './contexts/ProjectsContext';
 // ----------------------------------------------------------------------
 
 export default function App() {
@@ -25,6 +26,14 @@ export default function App() {
             <EmployeeProvider>
               <TeamsProvider>
                 <Router />
+                <AnimatedCursor
+                  innerSize={12}
+                  outerSize={32}
+                  color="0, 171, 85"
+                  outerAlpha={0.2}
+                  innerScale={1}
+                  outerScale={1}
+                />
               </TeamsProvider>
             </EmployeeProvider>
           </ProjectsContextProvider>
