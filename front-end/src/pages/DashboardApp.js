a3a90ecb1a91fb22692e759115d375bb867d232a
ayushsnofi <cosmosofficial53@gmail.com>
2022-01-22 20:56:19 +0530

    Sign in and register page changed

--- front-end/src/pages/DashboardApp.js
+++ front-end/src/pages/DashboardApp.js
@@ -1,24 +1,24 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
 // material
-import { Box, Grid, Container, Typography } from '@mui/material';
+import { Box, Grid, Container, Typography } from "@mui/material";
 
 // contexts
-import { TeamsProvider } from '../contexts/TeamsContext';
-import { loginContext } from '../contexts/LoginContext';
+import { TeamsProvider } from "../contexts/TeamsContext";
+import { loginContext } from "../contexts/LoginContext";
 
 // components
-import Page from '../components/Page';
+import Page from "../components/Page";
 import {
   Monthlyhours,
   AppBugReports,
   AppItemOrders,
   Weeklyhours,
-  SimpleContainer
-} from '../components/_dashboard/app';
-import EmployeeContainer from '../components/_dashboard/employee/EmployeeContainer';
-import PageHeader from '../components/PageHeader';
-import { Role } from '../_helpers/role';
+  SimpleContainer,
+} from "../components/_dashboard/app";
+import EmployeeContainer from "../components/_dashboard/employee/EmployeeContainer";
+import PageHeader from "../components/PageHeader";
+import { Role } from "../_helpers/role";
 
 // ----------------------------------------------------------------------
 
@@ -26,15 +26,15 @@ export default function DashboardApp() {
   const { loginC } = useContext(loginContext);
   console.log(Role.indexOf(loginC.userData.role));
   return (
-    <Page title="Dashboard | Minimal-UI">
+    <Page title="Screen Monitor | Meru Accounting">
       <Container maxWidth="lg">
         <PageHeader title="Dashboard" />
 
         <Grid container spacing={2}>
           {Role.indexOf(loginC.userData.role) === 2 ? (
-            <SimpleContainer sx={{ width: '100%' }} />
+            <SimpleContainer sx={{ width: "100%" }} />
           ) : (
-            <EmployeeContainer sx={{ width: '100%' }} />
+            <EmployeeContainer sx={{ width: "100%" }} />
           )}
         </Grid>
       </Container>
