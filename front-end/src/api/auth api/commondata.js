f59193045299640c247478aaeab89905d757b888
Bharat Kumar <kumarbharat661@gmail.com>
2022-01-22 21:13:15 +0530

    made the Notification functionality

--- front-end/src/api/auth api/commondata.js
+++ front-end/src/api/auth api/commondata.js
@@ -3,15 +3,10 @@ import {
   GET_COMMONDATA_SUCCESS,
   GET_COMMONDATA_FAILED,
 } from "../../constants/CurrentUserConstants";
-const config = {
-  headers: {
-    Authorization: `Bearer ${localStorage["Bearer Token"]}`,
-  },
-};
 
 export const getCommonData = async (dispatch) => {
   try {
-    const { data } = await axios.get(`/commondata`, config);
+    const { data } = await axios.get(`/commondata`);
     dispatch({
       type: GET_COMMONDATA_SUCCESS,
       payload: data,
--- front-end/src/components/Settings/Sidebar.js
+++ front-end/src/components/Settings/Sidebar.js
@@ -1,13 +1,13 @@
 /* eslint-disable consistent-return */
-import React from 'react';
-import { Paper, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import Box from '@mui/material/Box';
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import SettingsMain from './SettingsMain';
-import { convertString } from '../../contexts/UserContext';
+import React from "react";
+import { Paper, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import Box from "@mui/material/Box";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import SettingsMain from "./SettingsMain";
+import { convertString } from "../../contexts/UserContext";
 
 function TabPanel(props) {
   const { children, value, index, heading, subheading, ...other } = props;
@@ -32,25 +32,25 @@ function TabPanel(props) {
 TabPanel.propTypes = {
   children: PropTypes.node,
   index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired
+  value: PropTypes.number.isRequired,
 };
 
 function a11yProps(index) {
   return {
     id: `vertical-tab-${index}`,
-    'aria-controls': `vertical-tabpanel-${index}`
+    "aria-controls": `vertical-tabpanel-${index}`,
   };
 }
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: '750px',
-    width: '100%',
-    margin: 'auto',
-    display: 'grid',
-    gridTemplateColumns: '30% 70%',
-    backgroundColor: '#fdfdff'
-  }
+    height: "750px",
+    width: "100%",
+    margin: "auto",
+    display: "grid",
+    gridTemplateColumns: "30% 70%",
+    backgroundColor: "#fdfdff",
+  },
 }));
 
 export default function Sidebar() {
@@ -66,23 +66,23 @@ export default function Sidebar() {
 
   const effectiveArr = {
     ScreenShotPerHour:
-      'How frequently screenshots will be taken .This number is an average since screenshots are taken at random intervals',
-      AppsAndUrlTracking:
-      'Track How frequent;y they were using KeyBoard and Mouse.',
-      AppsAndUrlTracking:
-      'Track what applications your team members use and what websites they visit.',
+      "How frequently screenshots will be taken .This number is an average since screenshots are taken at random intervals",
+    AppsAndUrlTracking:
+      "Track How frequent;y they were using KeyBoard and Mouse.",
+    AppsAndUrlTracking:
+      "Track what applications your team members use and what websites they visit.",
     WeeklyTimeLimit:
-      'Number of hours your employees are allowed to work. The tracking will stop when the limit is reached.The time zone for the time limit is always UTC',
+      "Number of hours your employees are allowed to work. The tracking will stop when the limit is reached.The time zone for the time limit is always UTC",
     AutoPause:
-      'Tracking will automatically pause after the specified period of inactivity and will automatically resume when user becomes active again.',
+      "Tracking will automatically pause after the specified period of inactivity and will automatically resume when user becomes active again.",
     OfflineTime:
-      'Allow user to add time not tracked by the program to their timeline manually. It is often used to account for work away from a computer.',
+      "Allow user to add time not tracked by the program to their timeline manually. It is often used to account for work away from a computer.",
     NotifyUser:
-      'Every time a screenshot is taken – a small notification will pop up for a couple of seconds next to the system tray saying that a Screenshot was taken.',
+      "Every time a screenshot is taken – a small notification will pop up for a couple of seconds next to the system tray saying that a Screenshot was taken.",
     WeekStart:
-      'When does your week start? This will be used when showing totals for a week or setting weekly time limits.',
+      "When does your week start? This will be used when showing totals for a week or setting weekly time limits.",
     CurrencySymbol:
-      'The symbol (e.g. $, €, £) will be shown when you set hourly pay rates for your employees and everywhere where money is shown (like total amount spent today or on a specific project).',
+      "The symbol (e.g. $, €, £) will be shown when you set hourly pay rates for your employees and everywhere where money is shown (like total amount spent today or on a specific project).",
   };
 
   return (
@@ -91,18 +91,18 @@ export default function Sidebar() {
       <Box
         component="div"
         sx={{
-          margin: '10px',
-          maxHeight: '60vh',
-          height: 'auto'
+          margin: "10px",
+          maxHeight: "60vh",
+          height: "auto",
         }}
       >
         <Paper
           component="div"
           elevation={3}
           sx={{
-            overflowY: 'auto',
-            height: '100%',
-            position: 'relative'
+            overflowY: "auto",
+            height: "100%",
+            position: "relative",
           }}
         >
           <Tabs
@@ -111,14 +111,18 @@ export default function Sidebar() {
             value={value}
             onChange={handleChange}
             aria-label="Vertical tabs example"
-            sx={{ borderRight: 1, borderColor: 'divider' }}
+            sx={{ borderRight: 1, borderColor: "divider" }}
           >
             {Object.keys(effectiveArr).map((keyName, keyIndex) => (
               <Tab
                 sx={{}}
                 label={
                   <Typography
-                    sx={{ textAlign: 'left', width: '100%', fontWeight: 'Bold' }}
+                    sx={{
+                      textAlign: "left",
+                      width: "100%",
+                      fontWeight: "Bold",
+                    }}
                     variant="h6"
                   >
                     {convertString(keyName)}
@@ -135,14 +139,17 @@ export default function Sidebar() {
       </Box>
 
       {/* HEADER */}
-      <Box component="div" sx={{ margin: '10px 10px 10px 0', overflow: 'auto' }}>
+      <Box
+        component="div"
+        sx={{ margin: "10px 10px 10px 0", overflow: "auto" }}
+      >
         {/* grid container 40 60 */}
         <Paper
           component="div"
           elevation={3}
           sx={{
-            overflow: 'visible',
-            position: 'relative'
+            overflow: "visible",
+            position: "relative",
             // display: 'grid',
             // gridTemplateRows: '30% 70%'
           }}
