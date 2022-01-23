7ca28809489403c455592d5456389e1562858349
Prakhar <srivastava11735@gmail.com>
2022-01-23 21:53:49 +0530

    Roled Based Access Route

--- back-end/controllers/client.js
+++ back-end/controllers/client.js
@@ -1,32 +1,61 @@
-import Client from '../models/client.js';
-import Project from '../models/project.js';
-import asyncHandler from 'express-async-handler';
-import User from '../models/user.js';
+import Client from "../models/client.js";
+import Project from "../models/project.js";
+import asyncHandler from "express-async-handler";
+import User from "../models/user.js";
+import { AccessControl } from "accesscontrol";
+import { grantsObject } from "../utils/permissions.js";
+
+const ac = new AccessControl(grantsObject);
+
+/* --------------------- Role Based Access Permissions --------------------- */
+
+// ac.grant("owner")
+//   .createAny(["client", "team", "project", "members"])
+//   .deleteAny(["client", "team", "project", "members"])
+//   .readAny(["client", "team", "project", "members"])
+//   .updateAny(["client", "team", "project", "members"])
+//   // .readAny('profile', ['*', '!password'])
+//   .grant("manager")
+//   .readOwn(["client", "team", "project", "members", "reported"])
+//   .createOwn(["profile", "video"])
+//   .updateAny(["members"], "team", "project")
+//   .deleteOwn(["client", "team", "project", "members"])
+
+//   .grant("employee")
+//   .readOwn(["client", "team", "project"]);
+
+/* -------------------------------------------------------------------------- */
 
 // @desc    Create a new client
 // @route   POST /client
 // @access  Private
 
 const createClient = asyncHandler(async (req, res) => {
-  const manager = req.user;
-  const { name } = req.body;
-  try {
-    const client = new Client({ name });
+  const permission = ac.can(req.user.role).createOwn("client");
+  if (permission.granted) {
+    try {
+      const { name } = req.body;
+      const manager = req.user;
+      const client = new Client({ name });
 
-    if (!client) throw new Error('Error creating a new client');
+      if (!client) throw new Error("Error creating a new client");
 
-    client.createdBy = manager._id;
-    await client.save();
+      client.createdBy = manager._id;
+      await client.save();
 
-    manager.clients.push(client);
-    await manager.save();
+      manager.clients.push(client);
+      await manager.save();
 
-    res.status(201).json({
-      status: 'Successfully Created Client',
-      data: client,
-    });
-  } catch (error) {
-    throw new Error(error);
+      res.status(201).json({
+        status: "Successfully Created Client",
+        data: client,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  } else {
+    // resource is forbidden for this user/role
+    res.status(403).end("UnAuthorized");
   }
 });
 
@@ -35,38 +64,49 @@ const createClient = asyncHandler(async (req, res) => {
 // @access  Private
 
 const getClient = asyncHandler(async (req, res) => {
-  try {
-    const client = await Client.find({ manager: req.user._id }).populate([
-      {
-        path: 'projects',
-        populate: [
-          {
-            path: 'employees',
-            select: ['firstName', 'lastName', 'days', 'email', 'projects'],
-            populate: {
-              path: 'projects',
-              model: 'Project',
-              select: ['name', 'budgetTime'],
+  console.log(ac);
+  const permission = ac.can(req.user.role).readOwn("client", ["*"]);
+  console.log(permission);
+  if (permission.granted) {
+    try {
+      const client = await Client.find({ manager: req.user._id }).populate([
+        {
+          path: "projects",
+          populate: [
+            {
+              path: "employees",
+              select: ["firstName", "lastName", "days", "email", "projects"],
+              populate: {
+                path: "projects",
+                model: "Project",
+                select: ["name", "budgetTime"],
+              },
             },
-          },
-          { path: 'projectLeader', select: ['firstName', 'lastName', 'email'] },
-          { path: 'createdBy', select: ['firstName', 'lastName'] },
-        ],
-      },
-      { path: 'createdBy', select: ['firstName', 'lastName'] },
-    ]);
+            {
+              path: "projectLeader",
+              select: ["firstName", "lastName", "email"],
+            },
+            { path: "createdBy", select: ["firstName", "lastName"] },
+          ],
+        },
+        { path: "createdBy", select: ["firstName", "lastName"] },
+      ]);
 
-    if (!client) {
-      res.status(404);
-      throw new Error('No clients found');
+      if (!client) {
+        res.status(404);
+        throw new Error("No clients found");
+      }
+
+      res.status(200).json({
+        status: "Client fetched succesfully",
+        data: client,
+      });
+    } catch (error) {
+      throw new Error(error);
     }
-
-    res.status(200).json({
-      status: 'Client fetched succesfully',
-      data: client,
-    });
-  } catch (error) {
-    throw new Error(error);
+  } else {
+    // resource is forbidden for this user/role
+    res.status(403).end("UnAuthorized");
   }
 });
 
@@ -76,14 +116,14 @@ const getClient = asyncHandler(async (req, res) => {
 
 const getClientById = asyncHandler(async (req, res) => {
   const { id } = req.params;
-  const client = await Client.findById(id).populate('projects');
+  const client = await Client.findById(id).populate("projects");
   try {
     if (!client) {
       res.status(404);
-      throw new Error('Client not found');
+      throw new Error("Client not found");
     }
     res.status(200).json({
-      status: 'Client fetched succesfully',
+      status: "Client fetched succesfully",
       data: client,
     });
   } catch (error) {
@@ -96,20 +136,26 @@ const getClientById = asyncHandler(async (req, res) => {
 // @access  Private
 
 const editClient = asyncHandler(async (req, res) => {
-  try {
-    const client = await Client.findByIdAndUpdate(req.params.id, req.body);
+  const permission = ac.can(req.user.role).updateAny("client");
+  if (permission.granted) {
+    try {
+      const client = await Client.findByIdAndUpdate(req.params.id, req.body);
 
-    if (!client) {
-      res.status(404);
-      throw new Error('Client not found');
+      if (!client) {
+        res.status(404);
+        throw new Error("Client not found");
+      }
+
+      res.status(200).json({
+        status: "Successfully Updated Client",
+        data: client,
+      });
+    } catch (error) {
+      throw new Error(error);
     }
-
-    res.status(200).json({
-      status: 'Successfully Updated Client',
-      data: client,
-    });
-  } catch (error) {
-    throw new Error(error);
+  } else {
+    // resource is forbidden for this user/role
+    res.status(403).end("UnAuthorized");
   }
 });
 
@@ -118,68 +164,73 @@ const editClient = asyncHandler(async (req, res) => {
 // @access  Private
 
 const deleteClient = asyncHandler(async (req, res) => {
-  const clientId = req.params.id;
+  const permission = ac.can(req.user.role).deleteOwn("client");
+  if (permission.granted) {
+    const clientId = req.params.id;
+    try {
+      /* ---------------------------- // finding Client ---------------------------- */
+      const client = await Client.findById(clientId);
 
-  try {
-    /* ---------------------------- // finding Client ---------------------------- */
-    const client = await Client.findById(clientId);
-
-    if (!client) {
-      res.status(404);
-      throw new Error('Client not found');
-    }
-
-    const userId = client.createdBy;
-
-    /* ------------------ finding user to delete client in that ----------------- */
-
-    const user = await User.findById(userId);
-    if (user) {
-      user.clients.forEach((client, index) => {
-        if (client.toHexString() == clientId) {
-          user.clients.splice(index, 1);
-        }
-      });
-      await user.save();
-    }
-
-    /* ------------------------ deleting projects in user ------------------------ */
-    // taking projects from deleting client and deleting the projects and project field from their respective members
-
-    for (let i = 0; i < client.projects.length; i++) {
-      const projectId = client.projects[i];
-      const project = await Project.findById(projectId);
-
-      for (let j = 0; j < project.employees.length; j++) {
-        // Deleting projects reference from the emoployee
-        const employeeId = project.employees[j];
-        const employee = await User.findById(employeeId);
-
-        if (employee) {
-          employee.projects.forEach((project, index) => {
-            if (project.toHexString() === projectId.toHexString()) {
-              employee.projects.splice(index, 1);
-            }
-          });
-          await employee.save();
-        }
+      if (!client) {
+        res.status(404);
+        throw new Error("Client not found");
       }
-      // Deleting Project
-      await Project.findByIdAndRemove(projectId);
+
+      const userId = client.createdBy;
+
+      /* ------------------ finding user to delete client in that ----------------- */
+
+      const user = await User.findById(userId);
+      if (user) {
+        user.clients.forEach((client, index) => {
+          if (client.toHexString() == clientId) {
+            user.clients.splice(index, 1);
+          }
+        });
+        await user.save();
+      }
+
+      /* ------------------------ deleting projects in user ------------------------ */
+      // taking projects from deleting client and deleting the projects and project field from their respective members
+
+      for (let i = 0; i < client.projects.length; i++) {
+        const projectId = client.projects[i];
+        const project = await Project.findById(projectId);
+
+        for (let j = 0; j < project.employees.length; j++) {
+          // Deleting projects reference from the emoployee
+          const employeeId = project.employees[j];
+          const employee = await User.findById(employeeId);
+
+          if (employee) {
+            employee.projects.forEach((project, index) => {
+              if (project.toHexString() === projectId.toHexString()) {
+                employee.projects.splice(index, 1);
+              }
+            });
+            await employee.save();
+          }
+        }
+        // Deleting Project
+        await Project.findByIdAndRemove(projectId);
+      }
+
+      /* --------------------------- deleting the client -------------------------- */
+
+      await Client.findByIdAndRemove(clientId);
+
+      /* ---------------------------- Sending response ---------------------------- */
+
+      res.status(202).json({
+        status: "Successfully Deleted Client",
+        data: client,
+      });
+    } catch (error) {
+      throw new Error(error);
     }
-
-    /* --------------------------- deleting the client -------------------------- */
-
-    await Client.findByIdAndRemove(clientId);
-
-    /* ---------------------------- Sending response ---------------------------- */
-
-    res.status(202).json({
-      status: 'Successfully Deleted Client',
-      data: client,
-    });
-  } catch (error) {
-    throw new Error(error);
+  } else {
+    // resource is forbidden for this user/role
+    res.status(403).end("UnAuthorized");
   }
 });
 
--- back-end/controllers/employee.js
+++ back-end/controllers/employee.js
@@ -1,27 +1,38 @@
-import User from '../models/user.js';
-import Client from '../models/client.js';
-import Project from '../models/project.js';
-import Team from '../models/team.js';
-import asyncHandler from 'express-async-handler';
+import User from "../models/user.js";
+import Client from "../models/client.js";
+import Project from "../models/project.js";
+import Team from "../models/team.js";
+import asyncHandler from "express-async-handler";
+
+import { AccessControl } from "accesscontrol";
+import { grantsObject } from "../utils/permissions.js";
+
+const ac = new AccessControl(grantsObject);
 
 // @desc    Get employee details by ID
 // @route   GET /employee/:id
 // @access  Private
 
 const getEmployeeById = asyncHandler(async (req, res) => {
-  try {
-    const { id } = req.params;
-    const employee = await User.findById(id);
-    if (!employee) {
-      res.status(404);
-      throw new Error(`Employee not found `);
+  const permission = ac.can(req.user.role).readOwn("member");
+  if (permission.granted) {
+    try {
+      const { id } = req.params;
+      const employee = await User.findById(id);
+      if (!employee) {
+        res.status(404);
+        throw new Error(`Employee not found `);
+      }
+      res.status(200).json({
+        status: "Ok",
+        data: employee,
+      });
+    } catch (error) {
+      throw new Error(error);
     }
-    res.status(200).json({
-      status: 'Ok',
-      data: employee,
-    });
-  } catch (error) {
-    throw new Error(error);
+  } else {
+    // resource is forbidden for this user/role
+    res.status(403).end("UnAuthorized");
   }
 });
 
@@ -30,23 +41,29 @@ const getEmployeeById = asyncHandler(async (req, res) => {
 // @access  Private
 
 const getEmployeeList = asyncHandler(async (req, res) => {
-  try {
-    const { teams } = await User.findById(req.user._id).populate({
-      path: 'teams',
-      model: 'Team',
-      populate: {
-        path: 'members',
-        model: 'User',
-        select: ['firstName', 'lastName', 'email'],
-      },
-    });
+  const permission = ac.can(req.user.role).readOwn("member");
+  if (permission.granted) {
+    try {
+      const { teams } = await User.findById(req.user._id).populate({
+        path: "teams",
+        model: "Team",
+        populate: {
+          path: "members",
+          model: "User",
+          select: ["firstName", "lastName", "email"],
+        },
+      });
 
-    res.status(200).json({
-      messsage: 'Success',
-      data: teams,
-    });
-  } catch (error) {
-    throw new Error(error);
+      res.status(200).json({
+        messsage: "Success",
+        data: teams,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  } else {
+    // resource is forbidden for this user/role
+    res.status(403).end("UnAuthorized");
   }
 });
 
@@ -56,66 +73,72 @@ const getEmployeeList = asyncHandler(async (req, res) => {
 
 const deleteEmployee = asyncHandler(async (req, res) => {
   const id = req.params.id;
-  try {
-    /* ---------------------------- finding employee ---------------------------- */
+  const permission = ac.can(req.user.role).deleteOwn("member");
+  if (permission.granted) {
+    try {
+      /* ---------------------------- finding employee ---------------------------- */
 
-    const employee = await User.findById(id);
+      const employee = await User.findById(id);
 
-    if (employee) {
-      res.status(404);
-      throw new Error('Employee to be deleted not found');
-    }
-
-    /* ------ finding employee clients and removing createdBy field ----- */
-
-    for (let i = 0; i < employee.clients.length; i++) {
-      const clientId = employee.clients[i];
-      const client = await Client.findById(clientId);
-      client.createdBy = undefined;
-      await client.save();
-    }
-
-    /* ------ removing project reference field of employee that is deleted ------ */
-    for (let i = 0; i < employee.projects.length; i++) {
-      const projectId = employee.projects[i];
-      const project = await Project.findById(projectId);
-
-      project.employees.forEach((employee, index) => {
-        if (employee.toHexString() == id.toHexString()) {
-          project.employees.splice(index, 1);
-        }
-      });
-      if (project.projectLeader.toHexString() == id.toHexString()) {
-        project.projectLeader = undefined;
+      if (employee) {
+        res.status(404);
+        throw new Error("Employee to be deleted not found");
       }
-      if (project.createdBy.toHexString() == id.toHexString()) {
-        project.createdBy = undefined;
+
+      /* ------ finding employee clients and removing createdBy field ----- */
+
+      for (let i = 0; i < employee.clients.length; i++) {
+        const clientId = employee.clients[i];
+        const client = await Client.findById(clientId);
+        client.createdBy = undefined;
+        await client.save();
       }
-      await project.save();
-    }
 
-    /* ------ removing team reference field of employee that is deleted ------ */
-    for (let i = 0; i < employee.teams.length; i++) {
-      const teamId = employee.teams[i];
-      const team = await Team.findById(teamId);
+      /* ------ removing project reference field of employee that is deleted ------ */
+      for (let i = 0; i < employee.projects.length; i++) {
+        const projectId = employee.projects[i];
+        const project = await Project.findById(projectId);
 
-      team.members.forEach((employee, index) => {
-        if (employee.toHexString() == id.toHexString()) {
-          team.members.splice(index, 1);
+        project.employees.forEach((employee, index) => {
+          if (employee.toHexString() == id.toHexString()) {
+            project.employees.splice(index, 1);
+          }
+        });
+        if (project.projectLeader.toHexString() == id.toHexString()) {
+          project.projectLeader = undefined;
         }
+        if (project.createdBy.toHexString() == id.toHexString()) {
+          project.createdBy = undefined;
+        }
+        await project.save();
+      }
+
+      /* ------ removing team reference field of employee that is deleted ------ */
+      for (let i = 0; i < employee.teams.length; i++) {
+        const teamId = employee.teams[i];
+        const team = await Team.findById(teamId);
+
+        team.members.forEach((employee, index) => {
+          if (employee.toHexString() == id.toHexString()) {
+            team.members.splice(index, 1);
+          }
+        });
+
+        await team.save();
+      }
+      await User.findByIdAndRemove(id);
+
+      res.json({
+        status: "Employee Deleted",
+        data: employee,
       });
-
-      await team.save();
+    } catch (error) {
+      res.status(500);
+      throw new Error(error);
     }
-    await User.findByIdAndRemove(id);
-
-    res.json({
-      status: 'Employee Deleted',
-      data: employee,
-    });
-  } catch (error) {
-    res.status(500);
-    throw new Error(error);
+  } else {
+    // resource is forbidden for this user/role
+    res.status(403).end("UnAuthorized");
   }
 });
 
@@ -124,16 +147,27 @@ const deleteEmployee = asyncHandler(async (req, res) => {
 // @access  Private
 
 const editEmployee = asyncHandler(async (req, res) => {
-  const employeeId = req.params.id;
-  try {
-    const user = await User.findByIdAndUpdate(employeeId, req.body);
-    user.save();
-    res.json({
-      message: 'User Updated',
-      data: user,
-    });
-  } catch (error) {
-    throw new Error(error);
+  console.log("Inside Route");
+  const permission = ac.can(req.user.role).updateOwn("member");
+  // console.log(permission.Possession);
+  if (permission.granted) {
+    const employeeId = req.params.id;
+    const filteredBody = permission.filter(req.body);
+    console.log(filteredBody);
+    try {
+      const user = await User.findByIdAndUpdate(employeeId, filteredBody);
+      console.log(user);
+      user.save();
+      res.json({
+        message: "User Updated",
+        data: user,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  } else {
+    // resource is forbidden for this user/role
+    res.status(403).end("UnAuthorized");
   }
 });
 
