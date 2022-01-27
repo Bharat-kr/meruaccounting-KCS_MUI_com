6f1dd2bb0fe944eef19991fb21244e94cec531f0
MeruAccounting <92557451+meruaccounting@users.noreply.github.com>
2022-01-27 22:31:21 +0530

    procfile

--- back-end/app.js
+++ back-end/app.js
@@ -1,26 +1,26 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import colors from 'colors';
-import swaggerUi from 'swagger-ui-express';
-import path from 'path';
-import YAML from 'yamljs';
-import cors from 'cors';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import connectDB from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import colors from "colors";
+import swaggerUi from "swagger-ui-express";
+import path from "path";
+import YAML from "yamljs";
+import cors from "cors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
-import authRoutes from './routes/auth.js';
-import clientRoutes from './routes/client.js';
-import teamRoutes from './routes/team.js';
-import projectRoutes from './routes/project.js';
-import employeeRoutes from './routes/employee.js';
-import activityRoutes from './routes/activity.js';
-import reportRoutes from './routes/report.js';
-import uploadRoutes from './routes/upload.js';
-import notificationRoutes from './routes/notify.js';
-import avatarRoutes from './routes/avatar.js';
+import authRoutes from "./routes/auth.js";
+import clientRoutes from "./routes/client.js";
+import teamRoutes from "./routes/team.js";
+import projectRoutes from "./routes/project.js";
+import employeeRoutes from "./routes/employee.js";
+import activityRoutes from "./routes/activity.js";
+import reportRoutes from "./routes/report.js";
+import uploadRoutes from "./routes/upload.js";
+import notificationRoutes from "./routes/notify.js";
+import avatarRoutes from "./routes/avatar.js";
 
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "../.env" });
 
 connectDB();
 
@@ -31,27 +31,27 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
-const swaggerDocs = YAML.load('./api.yaml');
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+const swaggerDocs = YAML.load("./api.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-app.use('/employee', employeeRoutes);
-app.use('/', authRoutes);
-app.use('/team', teamRoutes);
-app.use('/client', clientRoutes);
-app.use('/project', projectRoutes);
-app.use('/activity', activityRoutes);
-app.use('/upload', uploadRoutes);
-app.use('/avatar', avatarRoutes);
-app.use('/report', reportRoutes);
-app.use('/notify', notificationRoutes);
+app.use("/employee", employeeRoutes);
+app.use("/", authRoutes);
+app.use("/team", teamRoutes);
+app.use("/client", clientRoutes);
+app.use("/project", projectRoutes);
+app.use("/activity", activityRoutes);
+app.use("/upload", uploadRoutes);
+app.use("/avatar", avatarRoutes);
+app.use("/report", reportRoutes);
+app.use("/notify", notificationRoutes);
 
 const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.use('/avatar', express.static(path.join(__dirname, '/avatar')));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use("/avatar", express.static(path.join(__dirname, "/avatar")));
 
 // Middleware
 app.use(notFound);
