c6d746dd5e43c4e252481967c1985444472f09ee
MeruAccounting <92557451+meruaccounting@users.noreply.github.com>
2022-01-28 14:57:05 +0530

    app.js heroku

--- back-end/app.js
+++ back-end/app.js
@@ -21,9 +21,10 @@ import notificationRoutes from "./routes/notify.js";
 import avatarRoutes from "./routes/avatar.js";
 
 const __dirname = path.resolve();
-dotenv.config({ path: path.join(__dirname, "../.env") }); //local
 
-// dotenv.config({ path: path.join(__dirname, "./.env") }); //heroku
+// dotenv.config({ path: path.join(__dirname, "../.env") }); //local
+
+dotenv.config({ path: path.join(__dirname, "./.env") }); //heroku
 
 connectDB();
 
