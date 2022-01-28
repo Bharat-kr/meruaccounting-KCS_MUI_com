1d24f576e2d19da9f10704188791d6e398a89ee0
Ayush Garg <ayush181000@gmail.com>
2022-01-28 20:12:04 +0530

    more report features added

--- back-end/routes/report.js
+++ back-end/routes/report.js
@@ -1,16 +1,20 @@
 import express from "express";
 import {
-  generateReportOne,
-  generateReportTwo,
-  generateReportThree,
+  generateReport,
+  generateReportProject,
+  generateReportClient,
+  generateReportByUser,
+  generateReportByProjectId,
 } from "../controllers/report.js";
 
 import { authPass } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/1").get(authPass, generateReportOne);
-router.route("/2").get(authPass, generateReportTwo);
-router.route("/3").get(authPass, generateReportThree);
+router.route("/").get(authPass, generateReport);
+router.route("/project").get(authPass, generateReportProject);
+router.route("/client").get(authPass, generateReportClient);
+router.route("/user").get(authPass, generateReportByUser);
+router.route("/:id").get(authPass, generateReportByProjectId);
 
 export default router;
