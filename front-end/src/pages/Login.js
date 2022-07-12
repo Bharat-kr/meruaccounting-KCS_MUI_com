826cb57ffbf1fe1888a19e2e2e375d14d2b7a492
MeruAccounting <92557451+meruaccounting@users.noreply.github.com>
2022-07-12 17:28:25 +0530

    authsocial removed

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -92,7 +92,7 @@ export default function Login() {
               Enter your details below.
             </Typography>
           </Stack>
-          <AuthSocial />
+          {/* <AuthSocial /> */}
 
           <LoginForm />
 
