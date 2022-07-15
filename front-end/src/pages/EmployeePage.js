1dbc84d194f205426134607211588c9e302bea3d
Bharat Kumar <kumarbharat661@gmail.com>
2022-07-15 21:02:01 +0530

    added serach bar on employepage

--- front-end/src/pages/EmployeePage.js
+++ front-end/src/pages/EmployeePage.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, useContext } from "react";
-import { CssBaseline, Box } from "@mui/material";
+import React, { useState, useEffect, useContext, useLayoutEffect } from "react";
+import { CssBaseline, Box, Autocomplete, TextField } from "@mui/material";
 import moment from "moment";
-import { useParams, useLocation } from "react-router-dom";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
 
 // components
 import Calendar from "../components/EmployeePage/Calendar";
@@ -18,12 +18,18 @@ import { EmployeePageContext } from "src/contexts/EmployeePageContext";
 import { getCommonData } from "../api/employee api/employeePage";
 import { getFullName } from "src/_helpers/getFullName";
 import OfflineTime from "src/components/EmployeePage/OfflineTime";
+import { getTeamCommonData } from "src/api/auth api/commondata";
+import { CurrentUserContext } from "src/contexts/CurrentUserContext";
 
 export default function UserPage() {
   const [activities, setactivities] = useState([]);
   const { commonData, dispatchCommonData } = useContext(EmployeePageContext);
+  const navigate = useNavigate();
+  const { dispatchTeamCommonData, teamCommonData } =
+    useContext(CurrentUserContext);
   const [isInternal, setisInternal] = useState(false);
   const { id } = useParams();
+  const [teamsList, setTeamsList] = useState([]);
   const location = useLocation();
   const [date, setdate] = useState(
     moment()
@@ -33,7 +39,21 @@ export default function UserPage() {
   const [dateObj, setdateObj] = useState(
     moment().subtract(location.state === "yesterday" ? 1 : 0, "day")
   );
+  useLayoutEffect(() => {
+    let pushData = [];
+    let int = setInterval(() => {
+      getTeamCommonData(dispatchTeamCommonData);
+    }, [120000]);
 
+    teamCommonData?.data?.data?.forEach((mem) => {
+      pushData.push({
+        id: mem._id,
+        Employee: `${mem.firstName} ${mem.lastName}`,
+      });
+    });
+    setTeamsList(pushData);
+    return () => clearInterval(int);
+  }, []);
   // interval for getting common data each minute
   useEffect(() => {
     getCommonData(id, dispatchCommonData);
@@ -41,7 +61,7 @@ export default function UserPage() {
       getCommonData(id, dispatchCommonData);
     }, 60000);
     return () => clearInterval(cDataInterval);
-  }, []);
+  }, [id]);
 
   // filter out activities
   useEffect(() => {
@@ -61,6 +81,18 @@ export default function UserPage() {
     }
   }, [commonData, isInternal, date]);
 
+  const handleSearch = (e, value) => {
+    const member = teamsList.filter((member) =>
+      member.Employee === value ? member : ""
+    );
+    if (member.length === 0) {
+      // eslint-disable-next-line no-useless-return
+      return;
+    } else {
+      navigate(`/dashboard/employeepage/${member[0].id}`);
+    }
+  };
+
   return (
     <CssBaseline>
       <Box component="div" sx={{ width: "95%", margin: "auto" }}>
@@ -70,6 +102,31 @@ export default function UserPage() {
             commonData?.commonData?.user?.lastName
           )}
         />
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "flex-end",
+            mb: 4,
+            pr:2
+          }}
+        >
+          <Autocomplete
+            disablePortal
+            onChange={handleSearch}
+            id="employee-search"
+            options={teamsList.map((element) => element.Employee)}
+            sx={{
+              width: 300,
+              height: 20,
+              pb: 3,
+              mb: 2,
+            }}
+            renderInput={(params) => (
+              <TextField {...params} label="Search Employee" />
+            )}
+          />
+        </Box>
         <Calendar
           days={commonData?.commonData?.user?.days}
           date={dateObj.format("D")}
